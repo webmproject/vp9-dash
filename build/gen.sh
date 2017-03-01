41d2780c86b909dd471d3ea665c5bcfd529a7ac5
Lou Quillio <louquillio@google.com>
2017-03-01 12:25:53 -0800

    Tweaks to HTML/PDF generation script.

--- build/gen.sh
+++ build/gen.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 ## Requires kramdown and wkhtmltopdf
-infile='../VPCodecISOMediaFileFormatBinding.md'
+infile='VPCodecISOMediaFileFormatBinding.md'
 outfile='vp-codec-iso-media-file-format-binding-latest'
 
 printf "\nConverting Markdown to HTML ...\n"
 
-kramdown --template html-template ${infile} | \
+kramdown --template html-template ../${infile} | \
   # Change image path to parent, and make empty <th> truly empty
   sed -e 's/src="images/src="\.\.\/images/g' \
   -re 's|<th>\xc2\xa0</th>|<th></th>|g' \
@@ -15,6 +15,6 @@ printf "\nHTML finished.\n\n"
 
 printf "\nConverting HTML to PDF ...\n\n"
 
-wkhtmltopdf vp-codec-iso-media-file-format-binding-latest.html ${outfile}.pdf
+wkhtmltopdf ${outfile}.html ${outfile}.pdf
 
 printf "\nPDF finished.\n\n"
